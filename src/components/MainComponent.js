27ed280f9a598e83b1a7c3ca04e89da6e7178946
Nastasha Leach <nastasha.leach@outlook.com>
2021-05-31 12:17:11 -0500

    053121 12PM Added Redux actions and Reducer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import About from "./AboutComponent";
 import{ Switch, Route, Redirect, withRouter} from "react-router-dom";
 import { connect } from "react-redux";
 import Contact from "./ContactComponent";
+import { addComment } from "../redux/ActionCreators";
 
 
 const mapStateToProps = state => {
@@ -19,6 +20,11 @@ const mapStateToProps = state => {
         promotions: state.promotions
     }
 }
+
+const mapDispatchToProps = {
+    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text))
+};
+
 class Main extends Component {
    
 
@@ -39,7 +45,7 @@ class Main extends Component {
                 <CampsiteInfo 
                     campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} 
                     comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
-                    
+                    addComment={this.props.addComment}
                 />
             );
         }
@@ -61,4 +67,4 @@ class Main extends Component {
     }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
