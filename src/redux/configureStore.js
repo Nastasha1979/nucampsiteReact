bbbc4c9b82ae6de2b32cc4c04177cb518644ba7e
Nastasha Leach <nastasha.leach@outlook.com>
2021-05-31 13:04:51 -0500

    053121 104PM Added Logger and Thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,11 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 import { Campsites } from "./campsites";
 import { Comments } from "./comments";
 import { Promotions } from "./promotions";
 import { Partners } from "./partners";
+
 
 
 export const ConfigureStore = () => {
@@ -12,7 +15,8 @@ export const ConfigureStore = () => {
             comments: Comments,
             partners: Partners,
             promotions: Promotions
-        })
+        }),
+        applyMiddleware(thunk, logger)
     );
     return store;
 }
